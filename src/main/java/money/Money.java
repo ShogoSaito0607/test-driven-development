39dc7fbc7644c7cd55b5c1f4084780bf9467997a
Shogo Saito <shogo.saito@bitkey.jp>
2021-12-09 23:15:12 +0900

    commit: finish first section

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -8,10 +8,10 @@ public class Money implements Expression {
         this.amount = amount;
         this.currency = currency;
     }
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
     public Money reduce(Bank bank, String to) {
